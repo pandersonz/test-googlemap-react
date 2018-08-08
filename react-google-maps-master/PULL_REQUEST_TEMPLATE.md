6d7eb21667c53701c62ddca00de0c645fcc6529c
ork23 <p.anderson2390@gmail.com>
2018-08-07 23:13:12 -0400

    add: googlemapsapi

--- react-google-maps-master/PULL_REQUEST_TEMPLATE.md
+++
@@ -1,7 +0,0 @@
-## PLEASE CHECK "Allow edits from maintainers"
-
-![help](https://help.github.com/assets/images/help/pull_requests/allow-maintainers-to-make-edits.png)
-
-## FOLLOW "Conventional Commits Specification" FOR ALL COMMITS
-
-https://conventionalcommits.org/
--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
-import { GoogleMap, Marker } from "../react-google-maps-master"
+
 import logo from './logo.svg';
 import './App.css';
+const loadGoogleMapsApi = require('load-google-maps-api');
 
+loadGoogleMapsApi(Option[key]="AIzaSyCl6L-H0dwFPRbbynu1oOyTIM0mT0nGh60").then(function (googleMaps) {
+  new googleMaps.Map(document.querySelector('.map'), {
+    center: {
+      lat: 40.7484405,
+      lng: -73.9944191
+    },
+    zoom: 12
+  })
+}).catch(function (error) {
+  console.error(error)
+})
 class App extends Component {
   render() {
     return (
@@ -14,16 +26,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-        <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-      >
-        {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-      </GoogleMap>
-    
-    <MyMapComponent isMarkerShown />// Map with a Marker
-    <MyMapComponent isMarkerShown={false} />// Just only Map
+        
         </p>
+        <div className="map"></div>
       </div>
     );
   }
